3f5a83fc9d897bd633f6e2ac1a96a550a1d0fcd6
itc-s24013 <s24013@std.it-college.ac.jp>
2024-07-24 07:54:51 +0000

    p257~p277/2024/07/24

---
+++ 5-9.py
@@ -0,0 +1,19 @@
+mountain_in_japan = {'fuji': 3776, 'kitadake': 3193, 'okuhodakadake': 3190}
+
+mountain_in_japan_sorted = sorted(mountain_in_japan.items(), key=lambda x: x[0])
+
+for key, val in mountain_in_japan_sorted:
+    print(key, val)
+
+
+mountain_in_japan_sorted_1 = sorted(mountain_in_japan.items(), key=lambda x: x[1])
+
+for key, val in mountain_in_japan_sorted_1:
+    print(key,val)
+
+
+mountain_in_japan_sorted_1 = sorted(mountain_in_japan.items(), key=lambda x: x[1], reverse=True)
+
+for key, val in mountain_in_japan_sorted_1:
+    print(key,val)
+
