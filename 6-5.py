c610e36e23f2fe5b5fc64aa1a7fb7c163a7e60ab
itc-s24013 <s24013@std.it-college.ac.jp>
2024-08-06 08:01:28 +0000

    2024/08/06/p326~p341

---
+++ 6-5.py
@@ -0,0 +1,3 @@
+import my_module2
+m2 = my_module2.MyClass(746,739)
+m2.show_attributes()
---
+++ my_module1.py
@@ -0,0 +1,14 @@
+def func(v):
+    i = v + 3
+    return i
+
+class MyClass:
+    def __init__(self, a=1, b=3):
+        self.a = a
+        self.b = b
+
+    def show_attributes(self):
+        print("a = {}, b = {}, sum: {}".format(self.a, self.b, self.sum))
+
+    def sum(self):
+        return self.a + self.b
